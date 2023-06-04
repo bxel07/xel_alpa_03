61190663069aa42b26b982a214fd3adc6b1cdb3d
bxel <yogipratama.ut@gmail.com>
2023-06-05 04:42:42 +0700

    update

--- router/webroute.php
+++ router/webroute.php
@@ -11,26 +11,5 @@ Router::add("GET",'/product',\xel\framework\Service\Product::class,'categories')
 Router::add('GET','/',\xel\framework\Service\HomeController::class,'index', [Authmiddleware::class]);
 Router::add('POST','/post',\xel\framework\Service\HomeController::class,'hello');
 Router::add("POST",'/products',\xel\framework\Service\Product::class,'create');
-//Router::Add("POST", /);
 
-//run the invoke url 
 Router::run(); 
-
-/*
-
-get
-post
-put // patch
-delete
-
-
-
-Router->get('/', '', '');
-Router->get()->invoke_url()->class()->function();
-Router->run();
-
-Router->middeware([]) {
-    Router->get('/', '', '');
-};
-
-*/
